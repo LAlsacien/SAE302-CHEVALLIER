e1bf53dfea192da4a82bdf25c2d0c4d56589a551
LAlsacien <97260728+LAlsacien@users.noreply.github.com>
2022-11-27 18:00:14 +0100

    serveur n°1 fonctionnel + client se connecte au serveur

--- client.py
+++ client.py
@@ -1,7 +1,8 @@
 import sys
 from PyQt5.QtWidgets import *
-import threading
+import socket
 import requests
+import ipaddress
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -17,9 +18,14 @@ class MainWindow(QMainWindow):
         self.buttip = QPushButton("IP")
         self.buttname = QPushButton("Nom")
         self.chat = QTextEdit()
-        self.commands = QTextEdit()
+        self.commands = QLineEdit()
         self.commands.setMaximumHeight(25)
         self.envoyer = QPushButton("Envoyer")
+        self.conn = QLabel("IP du Serveur :")
+        self.ip = QLineEdit()
+        self.conn2 = QPushButton("Se connecter")
+        self.conn2.clicked.connect(self.connect)
+        self.ip.setMaximumHeight(25)
         grid.addWidget(self.inf, 0, 0)
         grid.addWidget(self.buttos, 1,0)
         grid.addWidget(self.buttram, 1, 1)
@@ -29,6 +35,9 @@ class MainWindow(QMainWindow):
         grid.addWidget(self.chat, 2, 0, 3, 5)
         grid.addWidget(self.commands, 5, 0, 1, 4)
         grid.addWidget(self.envoyer, 5, 4)
+        grid.addWidget(self.conn, 6, 0)
+        grid.addWidget(self.ip, 6, 1, 1, 2)
+        grid.addWidget(self.conn2, 6, 3)
         widget.setLayout(grid)
         self.setWindowTitle("Gestionnaire de Serveurs")
 
@@ -53,6 +62,34 @@ class MainWindow(QMainWindow):
         Menu.addAction(disconnect)
         Menu.addAction(kill)
         Menu.addAction(reset)
+
+    def connect(self):
+        socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            host = ipaddress.ip_address(self.ip.text())
+        except ValueError:
+            msg = QMessageBox()
+            msg.setText("Erreur !")
+            msg.setInformativeText('Vous n\'avez pas entrée une IP correcte.')
+            msg.setWindowTitle("Erreur !")
+            msg.exec_()
+        else:
+            try:
+                host = str(self.ip.text())
+                socket_client.connect((host, 10000))
+            except ConnectionRefusedError:
+                msg = QMessageBox()
+                msg.setText("Erreur !")
+                msg.setInformativeText('Connexion refusée.')
+                msg.setWindowTitle("Erreur !")
+                msg.exec_()
+            else:
+                pseudo, entrer = QInputDialog.getText(self, "Connexion réussie.", "Veuillez entrer votre pseudo :", QLineEdit.Normal, "")
+                if entrer:
+                    pseudo = pseudo
+                socket_client.send(pseudo.encode())
+                serverpseudo = socket_client.recv(1024).decode()
+                
     
 app = QApplication.instance() 
 
